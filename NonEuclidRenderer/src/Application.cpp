61742e2d2949ff99bb6d1b033254fae79b7e6b62
jxc1389 <jxc1389@student.bham.ac.uk>
2023-11-28 00:25:32 +0000

    ep17done

--- NonEuclidRenderer/src/Application.cpp
+++ NonEuclidRenderer/src/Application.cpp
@@ -9,9 +9,11 @@
 #include "Renderer.h" 
 
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "Texture.h"
 
 int main(void){
     GLFWwindow* window;
@@ -44,10 +46,10 @@ int main(void){
 
     {
         float positions[] = {
-            -0.5f, -0.5f,
-             0.5f, -0.5f,
-             0.5f,  0.5f,
-            -0.5f,  0.5f
+            -0.5f, -0.5f,   0.0f, 0.0f,
+             0.5f, -0.5f,   1.0f, 0.0f,
+             0.5f,  0.5f,   1.0f, 1.0f,
+            -0.5f,  0.5f,   0.0f, 1.0f
         };
 
         unsigned int indices[] = {
@@ -55,10 +57,14 @@ int main(void){
             2, 3, 0
         };
 
+        GLCall(glEnable(GL_BLEND));
+        GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
         VertexArray va;
-        VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
         
         VertexBufferLayout layout;
+        layout.Push<float>(2);
         layout.Push<float>(2);
         va.AddBuffer(vb, layout); 
         
@@ -68,10 +74,16 @@ int main(void){
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
+        Texture texture("res/textures/smile.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
+
         va.Unbind();
         shader.Unbind();
         vb.Unbind();
         ib.Unbind();
+
+        Renderer renderer;
 
         float r = 0.0f;
         float increment = 0.05f;
@@ -80,15 +92,12 @@ int main(void){
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            GLCall(glClear(GL_COLOR_BUFFER_BIT));
+            renderer.Clear();
 
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
-            va.Bind();
-            ib.Bind();
-
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+            renderer.Draw(va, ib, shader);
 
             if (r > 1.0f)
                 increment = -0.05f;
